3fe8b0b9cfad385933fbb47e80997d833aa05bd3
kagb <ka@evilka.com>
2016-09-08 18:50:04 +0800

    fix fetch

--- xarticle.py
+++ xarticle.py
@@ -23,18 +23,22 @@ class XArticle(object):
         title = self.doc.xpath('//title[position()=1]/text()')
         self.title = title[0] if title else u'None Title'
 
-    def fetch(self, url, headers={}):
+    def fetch(self, url, headers={}, **kwargs):
         self.__init__()
         self.url = url
-        content = self.req_content(url, headers=headers)
+        content = self.req_content(url, headers=headers, **kwargs)
         self.html = self.decode_content(content)
         self.doc = document_fromstring(self.html)
 
     @classmethod
-    def req_content(cls, url, **kwargs):
-        headers = kwargs.get('headers', {})
-        r = requests.get(url, headers=headers, timeout=10, verify=False)
-        return r.content
+    def req_content(cls, url, headers={}, timeout=10, verify=False, **kwargs):
+        r = requests.get(url,
+                         headers=headers,
+                         timeout=timeout,
+                         verify=verify,
+                         **kwargs)
+        content = r.content if r.status_code == 200 else ''
+        return content
 
     @classmethod
     def decode_content(cls, content):
