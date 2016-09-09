1f562777c3dc4865867913cc26658bd3a5bb8e54
kagb <ka@evilka.com>
2016-09-09 14:01:38 +0800

    setup pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='xarticle',
+  packages=['xarticle'],
+  version='0.1.0',
+  description='A lite extractor for web page',
+  author='kagb',
+  author_email='ka@evilka.com',
+  url='https://github.com/kagb/xarticle',
+  download_url='https://github.com/kagb/xarticle/tarball/0.1.0',
+  keywords=['xarticle', 'extractor', 'html'],
+  classifiers=[],
+)
