a419129223fa300bf4bdf1ad34f537dd9b7b8f70
kagb <ka@evilka.com>
2016-09-09 13:38:17 +0800

    add sitemap

---
+++ sitemap.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+SITE_MAP = {
+    'default': {
+        'title': '//title[position()=1]/text()',
+        'pics': '//img/@src',
+        'videos': '//video/@src',
+    },
+    'm.weibo.cn': {
+    },
+}
