e758850fb80b2fea796d06462f172c07127c4c74
kagb <ka@evilka.com>
2016-09-14 17:35:49 +0800

    version 0.1.31

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='xarticle',
   packages=['xarticle'],
-  version='0.1.3',
+  version='0.1.31',
   description='A lite extractor for web page',
   author='kagb',
   author_email='ka@evilka.com',
   url='https://github.com/kagb/xarticle',
-  download_url='https://github.com/kagb/xarticle/tarball/0.1.3',
+  download_url='https://github.com/kagb/xarticle/tarball/0.1.31',
   keywords=['xarticle', 'extractor', 'html'],
   classifiers=[],
 )
